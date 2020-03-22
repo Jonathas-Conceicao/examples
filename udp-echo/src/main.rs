3e27070db8563ac897b62a614961eda8a2ab009b
Jonathas-Conceicao <jadoliveira@inf.ufpel.edu.br>
2020-03-22 04:37:05 -0300

    WIP: Yet another solution with unsafe pointer deref
    Signed-off-by: Jonathas-Conceicao <jadoliveira@inf.ufpel.edu.br>

--- udp-echo/src/main.rs
+++ udp-echo/src/main.rs
@@ -1,4 +1,4 @@
-use actix::{fut::WrapFuture, Actor, ActorFuture, AsyncContext, Context, StreamHandler};
+use actix::{fut::WrapFuture, Actor, AsyncContext, Context, StreamHandler};
 use bytes::{Bytes, BytesMut};
 use futures_util::sink::SinkExt;
 use futures_util::stream::{SplitSink, StreamExt};
@@ -7,8 +7,10 @@ use tokio::net::UdpSocket;
 use tokio_util::codec::BytesCodec;
 use tokio_util::udp::UdpFramed;
 
+type MySink = SplitSink<UdpFramed<BytesCodec>, (Bytes, SocketAddr)>;
+
 struct UdpActor {
-    sink: SplitSink<UdpFramed<BytesCodec>, (Bytes, SocketAddr)>,
+    sink: MySink,
 }
 
 impl Actor for UdpActor {
@@ -19,13 +21,15 @@ struct UdpPacket(BytesMut, SocketAddr);
 impl StreamHandler<UdpPacket> for UdpActor {
     fn handle(&mut self, msg: UdpPacket, ctx: &mut Context<Self>) {
         println!("Received: ({:?}, {:?})", msg.0, msg.1);
+        let sink_ref: *mut MySink = &mut self.sink;
         ctx.spawn(
-            async {}
-                .into_actor(self)
-                .then(|_, this, _| {
-                    this.sink.send((msg.0.into(), msg.1)).into_actor(this)
-                })
-                .map(|res, _, _| res.unwrap()),
+            async move {
+                unsafe { sink_ref.as_mut().unwrap() }
+                    .send((msg.0.into(), msg.1))
+                    .await
+                    .unwrap()
+            }
+            .into_actor(self),
         );
     }
 }

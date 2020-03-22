3142625398946fe5cb65f22e7311275143a2087e
Jonathas-Conceicao <jadoliveira@inf.ufpel.edu.br>
2020-03-22 04:06:35 -0300

    WIP: Update udp-echo example
    Missing:
    - There's a well known lifetime problem for processing this future
    
    Signed-off-by: Jonathas-Conceicao <jadoliveira@inf.ufpel.edu.br>

--- udp-echo/src/main.rs
+++ udp-echo/src/main.rs
@@ -1,32 +1,45 @@
-use actix::{Actor, AsyncContext, Context, Message, StreamHandler};
-use bytes::BytesMut;
-use futures::stream::SplitSink;
-use futures::{Future, Sink, Stream};
+use actix::{fut::WrapFuture, Actor, ActorFuture, AsyncContext, Context, StreamHandler};
+use bytes::{Bytes, BytesMut};
+use futures_util::sink::SinkExt;
+use futures_util::stream::{SplitSink, StreamExt};
 use std::net::SocketAddr;
-use tokio::codec::BytesCodec;
-use tokio::net::{UdpFramed, UdpSocket};
+use tokio::net::UdpSocket;
+use tokio_util::codec::BytesCodec;
+use tokio_util::udp::UdpFramed;
 
 struct UdpActor {
-    sink: SplitSink<UdpFramed<BytesCodec>>,
+    sink: SplitSink<UdpFramed<BytesCodec>, (Bytes, SocketAddr)>,
 }
+
 impl Actor for UdpActor {
     type Context = Context<Self>;
 }
 
-#[derive(Message)]
 struct UdpPacket(BytesMut, SocketAddr);
-impl StreamHandler<UdpPacket, std::io::Error> for UdpActor {
-    fn handle(&mut self, msg: UdpPacket, _: &mut Context<Self>) {
+impl StreamHandler<UdpPacket> for UdpActor {
+    fn handle(&mut self, msg: UdpPacket, ctx: &mut Context<Self>) {
         println!("Received: ({:?}, {:?})", msg.0, msg.1);
-        (&mut self.sink).send((msg.0.into(), msg.1)).wait().unwrap();
+        ctx.spawn(
+            async {}
+                .into_actor(self)
+                .then(|_, this, _| {
+                    this.sink.send((msg.0.into(), msg.1)).into_actor(this)
+                })
+                .map(|res, _, _| res.unwrap()),
+        );
     }
 }
 
 fn main() {
-    let sys = actix::System::new("echo-udp");
+    let mut sys = actix::System::new("echo-udp");
 
     let addr: SocketAddr = "127.0.0.1:0".parse().unwrap();
-    let sock = UdpSocket::bind(&addr).unwrap();
+    let sock = sys
+        .block_on(async move {
+            let addr = addr;
+            UdpSocket::bind(&addr).await
+        })
+        .unwrap();
     println!(
         "Started udp server on: 127.0.0.1:{:?}",
         sock.local_addr().unwrap().port()
@@ -34,9 +47,13 @@ fn main() {
 
     let (sink, stream) = UdpFramed::new(sock, BytesCodec::new()).split();
     UdpActor::create(|ctx| {
-        ctx.add_stream(stream.map(|(data, sender)| UdpPacket(data, sender)));
-        UdpActor { sink: sink }
+        ctx.add_stream(
+            stream
+                .map(|res| res.unwrap())
+                .map(|(data, sender)| UdpPacket(data, sender)),
+        );
+        UdpActor { sink }
     });
 
-    std::process::exit(sys.run());
+    sys.run().unwrap()
 }
